f4895353a1b7eaeee815430ef1fac52ee80ae091
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-09-04 18:00:39 +0700

    FIX: todo-list & deploy in github pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ListItems from './ListItems.js'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+library.add(faTrash);
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      items: [],
+      currentItem: {
+        text: '',
+        key: ''
+      }
+    }
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem: {
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    if(newItem.text!==""){
+      const newItems=[...this.state.items, newItem];
+      this.setState({
+        items: newItems,
+        currentItem:{
+          text: '',
+          key:''
+        }
+      })
+    }
+  }
+
+  deleteItem(key){
+    const filteredItems = this.state.items.filter(item =>
+      item.key!==key);
+      this.setState({
+        items:filteredItems
+      })
+  }
+
+  setUpdate(text, key){
+    const items = this.state.items;
+    // eslint-disable-next-line
+    items.map(item => {
+      if(item.key === key){
+        item.text = text;
+      }
+    })
+    this.setState({
+      items: items
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <header>
+          <form id="todo-form" onSubmit={(e)=>{this.addItem(e)}}>
+            <input 
+              type="text" 
+              placeholder="Masukkan Text"
+              value={this.state.currentItem.text}
+              onChange={(e)=>this.handleInput(e)}
+            />
+            <button type="submit">Add</button>
+          </form>
+        </header>
+        <ListItems 
+          items = {this.state.items}
+          deleteItem = {(key) => {this.deleteItem(key)}}
+          setUpdate = {(text, key) => {this.setUpdate(text, key)}}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
