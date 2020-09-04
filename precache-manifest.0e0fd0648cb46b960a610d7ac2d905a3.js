11767051f8db0270ced24bf0b4bac78a2ce66078
AndreDitVir01 <novaandre.saputraadit@gmail.com>
2020-09-04 17:55:05 +0700

    Updates

---
+++ precache-manifest.0e0fd0648cb46b960a610d7ac2d905a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a9ca6a6852decdf68e1dc3e07d3a07",
+    "url": "/todo_list-react_js-web/index.html"
+  },
+  {
+    "revision": "1dd4795e52b9da22f6e1",
+    "url": "/todo_list-react_js-web/static/css/main.941e1843.chunk.css"
+  },
+  {
+    "revision": "e6e496bc987bac875278",
+    "url": "/todo_list-react_js-web/static/js/2.02532db7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_list-react_js-web/static/js/2.02532db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dd4795e52b9da22f6e1",
+    "url": "/todo_list-react_js-web/static/js/main.abe3049b.chunk.js"
+  },
+  {
+    "revision": "f2bd4c5142ed7fe42b47",
+    "url": "/todo_list-react_js-web/static/js/runtime-main.ae17de37.js"
+  }
+]);
